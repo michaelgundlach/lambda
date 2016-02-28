f245094c32c0218cd0c076f8808596314bee0245
Michael Gundlach <gundlach@gmail.com>
2016-02-28 00:14:13 +0000

    Simple reduction working woo

--- parser.exs
+++ parser.exs
@@ -1,8 +1,6 @@
 ExUnit.start
 
 defmodule Lambda do
-  use ExUnit.Case, async: true
-
   def tokenize(term), do: term |> String.codepoints
 
   defmacro is_var(x), do: quote(do: unquote(x) >= "a" and unquote(x) <= "z")
@@ -25,33 +23,70 @@ defmodule Lambda do
   def to_ast([" " |t], [acc | stack]        ), do: to_ast(t,         stack |> push( [acc, :A] ))
   def to_ast([x   |t], stack) when is_var(x) , do: to_ast(t,         stack |> push( x         ))
 
-  def reduce(["L", var, "." | t]) do
-    "TODO"
+  def reduce_once({:A, {:L, old, term}, new}), do: sub(old, new, term)
+  def reduce_once({op, t1, t2}), do: {op, reduce_once(t1), reduce_once(t2)}
+  def reduce_once(x) when is_var(x), do: x
+
+  def reduce(term), do: reduce(term, 100)
+  def reduce(term, 0), do: term
+  def reduce(term, count) do
+    case reduce_once(term) do
+      ^term -> term
+      new_term -> reduce(new_term, count - 1)
+    end
   end
+
+  def sub(old, new, {:A, t1, t2}), do: {:A, sub(old, new, t1), sub(old, new, t2)}
+  def sub(old, new, {:L, v,   t}), do: {:L, v, (if old == v, do: t, else: sub(old, new, t))}
+  def sub(old, new, var) when is_var(var), do: if var == old, do: new, else: var
 
   def parse(term), do: term |> tokenize |> to_ast
 
-  test "tokenize" do
-    assert Lambda.tokenize("Lx.x") == ["L", "x", ".", "x"]
-    assert Lambda.tokenize("Lx.x y") == ["L", "x", ".", "x", " ", "y"]
+end
+
+defmodule Lambda.Test do
+  use ExUnit.Case, async: true
+  import Lambda
+
+  defmacrop reduce_test(pre, post) do
+    quote do: assert reduce_once(parse(unquote(pre))) == parse(unquote(post))
   end
 
-  # test "reduce" do
-  #   assert Lambda.reduce(Lambda.tokenize("Lx.x y")) == "y"
-  # end
+  test "reduce" do
+    assert reduce(parse("(Lx.x x) (Lx.x x)")) == parse("(Lx.x x) (Lx.x x)")
+    assert reduce(parse("(Lx.Ly.x y) z w")) == parse("z w")
+  end
+  test "reduce_once" do
+    reduce_test "(Lx.x) y",   "y"
+    reduce_test "(Lx.z) y",   "z"
+    reduce_test "(Lx.x z) y", "y z"
+    reduce_test "(Lx.x x) y", "y y"
+    reduce_test "(Lx.z z) y", "z z"
+    reduce_test "(Lx.Lz.x z) y", "Lz.y z"
+    reduce_test "(Lx.Lz.x z) (Lx.x)", "Lz.(Lx.x) z"
+    reduce_test "x y", "x y"
+    omega = "(Lx.x x)"
+    oMEGA = "#{omega} #{omega}" # Uppercase omega
+    reduce_test oMEGA, oMEGA
+  end
+
+  test "tokenize" do
+    assert tokenize("Lx.x") == ["L", "x", ".", "x"]
+    assert tokenize("Lx.x y") == ["L", "x", ".", "x", " ", "y"]
+  end
 
   test "to_ast" do
     thing = {:A, {:L, "x", {:L, "y", "z"}}, {:L, "x", "x"}}
-    assert Lambda.to_ast(Lambda.tokenize("((Lx.((Ly.((z))))) ((Lx.(x))))")) == thing
-    assert Lambda.to_ast(Lambda.tokenize("(((Lx.((Ly.((z))))) ((Lx.(x)))))")) == thing
-    assert Lambda.to_ast(Lambda.tokenize("(Lx.Ly.z) (Lx.x)")) == thing
-    assert Lambda.to_ast(Lambda.tokenize("(Lx.x) (Lx.x)")) == {:A, {:L, "x", "x"}, {:L, "x", "x"}}
-    assert Lambda.to_ast(Lambda.tokenize("x y")) == {:A, "x", "y"}
-    assert Lambda.to_ast(Lambda.tokenize("Lx.x")) == {:L, "x", "x"}
-    assert Lambda.to_ast(Lambda.tokenize("(Lx.x)")) == {:L, "x", "x"}
+    assert to_ast(tokenize("((Lx.((Ly.((z))))) ((Lx.(x))))")) == thing
+    assert to_ast(tokenize("(((Lx.((Ly.((z))))) ((Lx.(x)))))")) == thing
+    assert to_ast(tokenize("(Lx.Ly.z) (Lx.x)")) == thing
+    assert to_ast(tokenize("(Lx.x) (Lx.x)")) == {:A, {:L, "x", "x"}, {:L, "x", "x"}}
+    assert to_ast(tokenize("x y")) == {:A, "x", "y"}
+    assert to_ast(tokenize("Lx.x")) == {:L, "x", "x"}
+    assert to_ast(tokenize("(Lx.x)")) == {:L, "x", "x"}
   end
 
-  # test "parse" do
-  #   assert Lambda.parse("Lx.x") == Lambda.tokenize("Lx.x") |> Lambda.to_ast
-  # end
+  test "parse" do
+    assert parse("Lx.x") == tokenize("Lx.x") |> to_ast
+  end
 end
